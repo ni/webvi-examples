981ec2e61c6ec3ba8f5c684cd0a82683fb91cefd
doczhivago <doc.zhivago@gmail.com>
2018-05-18 13:00:53 -0500

    Feedback Updates
    Feedback based on updates from Guillermo to JSLI and JavaScript comments.

--- JSLI Calculator Example/Calculator.gcomp/Add.js
+++ JSLI Calculator Example/Calculator.gcomp/Add.js
@@ -1,27 +1,21 @@
 // Example JavaScript for JSLI Calculator Example WebVI
 // Add with JSLI node using a more complex, external JavaScript function.
-
-(function() {
-	'use strict';
-	// Uncomment the below line to log info to the browser console
-	//console.log("Entered Add with JSLI function...");
+// This creates a function object, AddWithJSLI, and adds it as a property to the Window object. 
+// The AddWithJSLI is a function expression that will add the two numbers. 
+// If 'window.' is removed from 'window.AddWithJSLI' then the function 
+// is no longer accessible from the WebVI. This is because it is only in the scope of 
+// this anonymous function, and not the global scope of the browser. 
+// See https://developer.mozilla.org/en-US/docs/Web/API/Window
+// The anonymous function pattern ( (function() {}()); ) is used to prevent conflicts with other functions
+// that's why we are explicitly adding a property to window)
 	
-	// Create variables with default values. 
-	var num1=1;
-	var num2=1;
+(function() {
+	//Use Strict prevents silent and common JavaScript errors
+	'use strict';
 
-	// This creates a function object, AddWithJSLI, and assigns the function object to 
-	// the window object. If 'window.' is removed from 'window.AddWithJSLI' then the function 
-	// is no longer accessible from the WebVI. This is because it is only in the scope of 
-	// this anonymous function, and not the global scope of the browser. 
-	// See https://developer.mozilla.org/en-US/docs/Web/API/Window
 	window.AddWithJSLI = function(num1, num2)
-	{
-		// Uncomment the below line to log info to the browser console everytime the JSLI node is called.		
-		//console.log("In AddWithJSLI JS function... X=" + num1 + "Y = " + num2);
-		
+	{	
 		var sum = num1 + num2;
-
 		return sum;
 	};
 	
---
+++ JSLI Calculator Example/Calculator.gcomp/javascript/Multiply.js
@@ -0,0 +1,6 @@
+// Example JavaScript for JSLI Calculator Example WebVI
+// Multiply with JSLI using a simple, external JavaScript function.
+
+function MultiplyWithJSLI(x, y) {  
+    return x * y;
+}
