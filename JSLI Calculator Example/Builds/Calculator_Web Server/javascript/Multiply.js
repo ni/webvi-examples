73013f197db4401336ca5b6a3eeac7fb838403b1
doczhivago <doc.zhivago@gmail.com>
2018-05-21 08:16:49 -0500

    Diagram Style Updates
    Main VI Diagram Style Updates

--- JSLI Calculator Example/Builds/Calculator_Web Server/javascript/Multiply.js
+++ JSLI Calculator Example/Builds/Calculator_Web Server/javascript/Multiply.js
@@ -1,10 +1,10 @@
 // Example JavaScript for JSLI Calculator Example WebVI
 // Multiply with the JSLI using a simple JavaScript function.
 //
-// This example creates a named function declaration, MultiplyWithJSLI.
-// The function is accessible from the global scope, and is therefore accessible to the JSLI. 
-// Global scope allows the function to add accessed by an function on the web page.
-// You should minimize the usage of global variables and functions to avoid namescape conflicts and minimize memory usage.
+// This example creates a function declaration.
+// The function is accessible from the global scope and is therefore accessible to the JSLI. 
+// Global scope allows the function to add accessed by any other function running in the browser window.
+// You should minimize the use of global scope to avoid naming conflicts and other global-related issues.
 //
 // From more information see:
 // https://github.com/ni/webvi-examples
--- JSLI WebSocket Echo/EchoClient.gcomp/javascript/WebSocketWrapper.js
+++ JSLI WebSocket Echo/EchoClient.gcomp/javascript/WebSocketWrapper.js
@@ -1,11 +1,10 @@
 // Example JavaScript for JSLI WebSocket Echo WebVI
-// This file wraps functions used by WebSocket JSLI nodes.
-// The example allows only one connection to the server.
+// This file defines the functions used by the WebSocket JSLI nodes.
 //
-// This example creates several WebSocket function objects and adds 
-// the functions as a property to the Window object. 
+// This example creates several WebSocket function literals, and adds 
+// the functions as properties of the Window object. 
 // The functions are defined within an anonymous function ( (function(){}()); ) 
-// to prevent polluting the global namespace, and are explicitly addded as properties.   
+// as a closure to prevent polluting the global namespace with our variables.
 // For example, if 'window.' is removed from 'window.OpenWebSocket' then the function 
 // is no longer accessible from the WebVI. This is because it is only in the scope of 
 // this anonymous function, and not the global scope of the browser. 
@@ -13,9 +12,11 @@
 // From more information see:
 // https://github.com/ni/webvi-examples
 // https://developer.mozilla.org/en-US/docs/Web/API/Window
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 // https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
 
 (function (){
+	
 	//Use strict prevents silent and common JavaScript errors.
 	'use strict';
 	var webSocket = undefined;
@@ -24,29 +25,17 @@
 
 	// Open connection to the Echo server. This example only allows one connection to the Echo server.
 	// Create a new WebSocket object  with a connection to the echo server and add EventListener functions.
-	window.OpenWebSocket = function(URI){
-		// Open Echo Server if no URL is given
-		if (URI ===""){
-			URI = "ws://echo.websocket.org/";
-		} 
-		
+	window.OpenWebSocket = function(URI){		
 		if (webSocket === undefined){
+			// Open Echo Server if no URL is given
+			if (URI === ""){
+				URI = "ws://echo.websocket.org/";
+			} 			
 			webSocket = new WebSocket(URI);
 			webSocket.onopen = function(evt) { onOpen(evt) };
 			webSocket.onmessage = function(evt) { onMessage(evt) };
 			return  webSocket.readyState;
 		} 
-
-	};
-
-	//Close the WebSocket connection if the connection is open. 
-	window.CloseWebSocket = function(){
-		if (webSocket !== undefined){
-			webSocket.close();
-			webSocket = undefined;
-			return webSocket.readyState;
-		}
-		return 3;
 	};
 
 	// Send a message from LabVIEW. If the connection is opened, send a message to the Echo Server.
@@ -68,17 +57,27 @@
 		}
 	};
 	
+	//Close the WebSocket connection if the connection is open. 
+	window.CloseWebSocket = function(){
+		if (webSocket !== undefined){
+			webSocket.close();
+			webSocket = undefined;
+		}
+	};
+
 	window.CheckState = function(){
 		return webSocket.readyState;
 	};
 
 	// Set the receivedMessage to empty string.
-	// These EventListener functions are not accessible from the JSLI because they are not added to the window object and they are not in the global namespace.
+	// These EventListener functions are not accessible from the JSLI because they are not 
+	// added to the window object and they are not in the global namespace.
 	function onOpen(evt){
 			receivedMessage ="";
 	}
 
-	// When a new message is received, store the message in memory and set newMessage to true. LabVIEW will poll for new messages with the CheckMessage function. 
+	// When a new message is received, the message is stored in memory and set newMessage to true. 
+	// LabVIEW will poll for new messages with the CheckMessage function. 
 	function onMessage(evt){
 			newMessage = true; 
 			receivedMessage = evt.data;
